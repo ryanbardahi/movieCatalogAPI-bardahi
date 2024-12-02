6401e60a96b4ccb17d8329d5c97676d7bbec187e
ryan.bardahi <ryan.bardahi@gmail.com>
2024-12-02 14:25:48 +0800

    Add admin in login

--- controllers/user.js
+++ controllers/user.js
@@ -23,8 +23,13 @@ exports.loginUser = async (req, res) => {
             return res.status(401).json({ error: 'Invalid credentials' });
         }
 
-        const token = jwt.sign({ userId: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
-        res.status(200).json({ access: token });
+        const token = jwt.sign(
+            { userId: user._id, isAdmin: user.isAdmin },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: '1h' }
+        );
+
+        res.status(200).json({ access: token, isAdmin: user.isAdmin });
     } catch (error) {
         res.status(500).json({ error: 'Failed to login' });
     }
